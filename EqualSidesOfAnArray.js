042cafa45e46d29a68f9933a4fc2676de78329cd
Yasin Vəliyev <68180570+YasinVeliyev@users.noreply.github.com>
2021-12-03 21:31:55 +0400

    Update EqualSidesOfAnArray.js

--- EqualSidesOfAnArray.js
+++ EqualSidesOfAnArray.js
@@ -1,22 +1,22 @@
 function findEvenIndex(arr) {
-    let newArr=[...arr]
-  let sum=(arr)=>{
-      if(!arr.length){
-          return 0
-      }
-      let result=0;
-      for(let i of arr){
-          result+=i
-      }
-      return result
-  }
-  for(let i=0;i<arr.length;i++){
-      if(sum(arr.splice(0,i))===sum(arr.splice(1))){
-          return i
-      }
-      arr=[...newArr]
-  }
-  return -1
+    let newArr = [...arr];
+    let sum = (arr) => {
+        let result = 0;
+        if (!arr.length) {
+            return result;
+        }
+        for (let i of arr) {
+            result += i;
+        }
+        return result;
+    };
+    for (let i = 0; i < arr.length; i++) {
+        if (sum(arr.splice(0, i)) === sum(arr.splice(1))) {
+            return i;
+        }
+        arr = [...newArr];
+    }
+    return -1;
 }
 
-console.log(findEvenIndex([1,100,50,-51,1,1]))
+console.log(findEvenIndex([1, 100, 50, -51, 1, 1]));
