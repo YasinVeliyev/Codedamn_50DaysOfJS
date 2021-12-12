4cbaa237fcce7d3bba35525afc84ec442c7c113f
Yasin Vəliyev <68180570+YasinVeliyev@users.noreply.github.com>
2021-12-12 21:05:24 +0400

    Update greatestCommonDivisor.js
    Write a program to find the greatest common divisor (gcd) of two positive numbers.
    The greatest common divisor (GCD), also called the greatest common factor, of two numbers is the largest number that divides them both. For instance, the greatest common factor of 20 and 15 is 5, since 5 divides both 20 and 15 and no larger number has this property.

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -1,12 +1,10 @@
 function gcd(a, b) {
   let gcd = 0
-  if (a % b == 0) {
-    return b
+  let min = Math.min(a, b)
+  if (a % min == 0 && b % min == 0) {
+    return min
   }
-  if (b % a == 0) {
-    return a
-  }
-  for (let i = 1; i <= Math.min(a, b) / 2; i++) {
+  for (let i = 1; i <= min / 2; i++) {
     if (a % i == 0 && b % i == 0 && i > gcd) {
       gcd = i
     }
